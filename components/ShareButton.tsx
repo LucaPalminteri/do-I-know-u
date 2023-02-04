d3042a4b2b9326d571279c7d58f9e5df163a5903
LucaPalminteri <lucapalminteri02@gmail.com>
2023-02-04 10:49:02 -0300

    add share code button component

---
+++ components/ShareButton.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+
+function ShareButton({code}:{code:string}) {
+
+    const shareData = {
+        title: 'Te Conozco?',
+        text: 'Unete a la partida y juguemos juntos!',
+        url: `http://localhost:3000/game/${code}`
+      }
+    
+      const shareHandler = async () => {
+        try {
+          await navigator.share(shareData)
+        } catch(error) {
+          console.error(error);
+        }
+      }
+      
+  return (
+    <button onClick={shareHandler}>Compartir codigo</button>
+  )
+}
+
+export default ShareButton
