a276a5d2ae93345d55c5667f62e217a104a0b8df
LucaPalminteri <lucapalminteri02@gmail.com>
2023-02-08 21:39:55 -0300

    add code to the token

--- utils/token.ts
+++ utils/token.ts
@@ -1,16 +1,16 @@
 import { sign } from "jsonwebtoken";
 import { serialize } from "cookie";
 
-export function createToken(username: string): string {
+export function createToken(username: string, code: string): string {
 
     const token: string = sign(
         {
-          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, username
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, username, code
         },
         process.env.NEXT_PUBLIC_TOKEN_NAME!
       );
     
-    const serialized: string = serialize("token", token, {
+    const serialized: string = serialize(process.env.NEXT_PUBLIC_TOKEN_PUBLIC_NAME!, token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
@@ -22,7 +22,7 @@ export function createToken(username: string): string {
 }
 
 export function deleteToken(): string {
-    const serialized: string = serialize("token", null, {
+    const serialized: string = serialize(process.env.NEXT_PUBLIC_TOKEN_PUBLIC_NAME!, null, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
