acee001279d1d2a2581d22ccb72be77f7f3b6f92
LucaPalminteri <lucapalminteri02@gmail.com>
2023-03-05 22:44:16 -0300

    feat: handle creating new question)game when all players answered

--- pages/api/confirm-answer.ts
+++ pages/api/confirm-answer.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import supabase from "@/utils/supabase";
-import { player_question, model_player_question } from "@/types/types";
+import { player_question, model_player_question, questions_games } from "@/types/types";
 
 export default async function joinGame(
     req: NextApiRequest,
@@ -25,21 +25,28 @@ export default async function joinGame(
             
         if (error) res.status(500).json(error);
         if (data == null) return;
-        if (data[0].questions_games.length == 0) {
+
+        let [ game ] = data
+
+        if (game.questions_games.length == 0) {
             res.status(200).json({"message":"todos respondieron la pregunta"});
             return;
         }
 
-        player_question.player = data[0].players_games[0].id;
-        player_question.question = data[0].questions_games[0].id
+        player_question.player = game.players_games[0].id;
+        player_question.question = game.questions_games[0].id
 
-        let aa = await supabase
+        let players_questions = await supabase
             .from('players_questions')
             .select()
             .eq('player',player_question.player)
 
-        if( aa.data == undefined) return;
-        if (aa.data?.length > 0) res.status(200).json({"message": "The player already chose an answer"});
+        if( players_questions.data == undefined) return;
+        // TODO: cheeck if theres no players left to answer
+        // if (players_questions.data?.length > 0) {
+        //     res.status(200).json({"message": "The player already chose an answer"});
+        //     return;
+        // }
 
         await supabase
             .from("players_questions")
@@ -47,12 +54,31 @@ export default async function joinGame(
 
         await supabase
         .from('questions_games')
-        .update({answered_count: data[0].questions_games[0].answered_count + 1})
-        .eq('id', data[0].questions_games[0].id)
-        .select()
+        .update({answered_count: game.questions_games[0].answered_count + 1})
+        .eq('id', game.questions_games[0].id)
 
         // cheeck if the count is equal to the amount of players in the game, if its equal change the state of the question isReady to true
         // if the state of isReady changes it should generate a new question_game field
+        
+        // WHEN questions_games.answeres_count == games.players_count THEN questions_games.isReady = true AND new questions_games
+
+        if (game.players_count - 1 == game.questions_games[0].answered_count) {
+
+            let newQuestion = Math.floor(Math.random() * 16) + 5
+
+            while (newQuestion == game.questions_games[0].question_id) {
+                newQuestion = Math.floor(Math.random() * 16) + 5
+            }
+
+            await supabase  
+                .from('questions_games')
+                .update({isReady:true})
+                .eq('id',game.questions_games[0].id)
+
+            await supabase
+                .from('questions_games')
+                .insert({game_id:game.id,question_id: Math.floor(Math.random() * 16) + 5})
+        }
         
         res.status(200).json(data);
     } catch (error) {
